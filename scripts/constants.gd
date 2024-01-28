a5c7cc337648cd5b3862f2fbc9ff81a87f0604c1
MatheusMDX <matheus_henriquemdx@hotmail.com>
2024-01-27 21:18:17 -0300

    Update constants.gd

--- scripts/constants.gd
+++ scripts/constants.gd
@@ -6,14 +6,15 @@ class_name Constants
 
 # --- Constants ---
 const JokesTypes := {
-	"TOC_TOC": "toc_toc", "TROCADILHO": "trocadilho", "PIADINHAS": "piadinhas"}
+	"TOC_TOC": "toc_toc", "PIADINHAS": "piadinhas", "NOME": "nome", 
+	"ANIMAL": "animal", "OBJETO": "objeto"}
 
 const TEST_PLAYER_DATA := {
 	"name": "player", 
 	"sprite": "res://sprites/Player.png",
 	"max_health": 100,
-	"resistences": {JokesTypes.TOC_TOC: 1, JokesTypes.TROCADILHO: 1, 
-	JokesTypes.PIADINHAS: 1},
+	"resistences": {JokesTypes.TOC_TOC: 1, JokesTypes.PIADINHAS: 1, 
+	JokesTypes.NOME: 1, JokesTypes.ANIMAL: 1, JokesTypes.OBJETO: 1},
 	"draw_amount": 3, 
 	"max_locks": 1, 
 	"deck": []}
